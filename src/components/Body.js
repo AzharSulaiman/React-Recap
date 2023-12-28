f830c3d1931d16fdad17fadf483f63c67479a67f
Azhar Sulaiman <mdazharsulaiman@gmail.com>
2023-12-28 12:04:37 -0500

    data from API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,28 @@
-import RestaurantCard from './RestaurantCard'
-import resList from '../../utils/restaurantData';
-import { useState } from 'react';
+import RestaurantCard from './RestaurantCard';
+import Shimmer from './shimmer'
+// local data before API call
+// import resList from '../../utils/restaurantData';
+import { useState, useEffect } from 'react';
 
 const Body = () => {
-    const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+    const [listOfRestaurant, setListOfRestaurant] = useState([]);
+
+    useEffect(() => {
+       fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.113645&lng=72.8697339&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    
+        const json = await data.json();
+        console.log('resList', json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        setListOfRestaurant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    }
+
+    if(listOfRestaurant.length === 0){
+        return <Shimmer/>
+    }
+
     return (
       <div className="body">
         <button className="filter-btn" onClick={()=>{
